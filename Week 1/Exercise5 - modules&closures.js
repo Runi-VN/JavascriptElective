70373907be6da402ca467cadd3c570ced813e1be
Runi <Runiniclasen@hotmail.com>
2020-02-02 19:36:16 +0100

    Add ES6 example

--- Week 1/Exercise5 - modules&closures.js
+++ Week 1/Exercise5 - modules&closures.js
@@ -39,3 +39,39 @@ let testPerson = person(); //no constructor
 testPerson.setName('Peter');
 testPerson.setAge(45);
 console.log(testPerson.getInfo()); //Peter, 45
+
+/*
+3) Implement an ES6 class with a similar functionality as requested in part 2.
+Don't use getXX or setXX but use ES6 properties
+
+https://youtu.be/FkTgvvYjuUo?t=540
+*/
+
+class Person {
+	constructor(age, name) {
+		this._age = age;
+		this._name = name;
+	}
+	getInfo = function() {
+		return `${this._name}, ${this._age}`;
+	};
+
+	get age() {
+		return this._age;
+	}
+
+	set age(newAge) {
+		this._age = newAge;
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	set name(newName) {
+		this._name = newName;
+	}
+}
+
+const peter = new Person(45, 'Peter');
+console.log(peter.getInfo()); //Peter, 45
